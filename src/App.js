77078e940cc7dd0d4e55024c4704a33d664109dc
Sakshi Sonawane <sonawanesakshi610@gmail.com>
2021-02-03 22:41:39 +0530

    external css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+       <h2>React App</h2>
+
+    <p id="para"> External CSS by using id   </p>
+
+<h3 className="heading1"> External CSS by using class</h3>
     </div>
   );
 }
